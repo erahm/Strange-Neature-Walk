ae24ba64a40e3565041421593d5da6bd02d8c575
Erich Rahm <erahm84@gmail.com>
2025-11-30 14:45:37 -0700

    adds privileges for category and exhibit

--- backend/authz/userPrivileges.js
+++ backend/authz/userPrivileges.js
@@ -6,27 +6,21 @@ export function defineAbilitiesForUser(user) {
   const privileges = privilegeSets(can, cannot);
   if (!user) {
     // unauthenticated users can read User listings
-    // cannot('read', 'User');
-    // cannot('create', 'User');
-    // cannot('update', 'User');
-    // cannot('delete', 'User');
     privileges.noAccess('User');
+    privileges.readOnly('Category');
+    privileges.readOnly('Exhibit');
   } else if (user.role === 'ADMIN') {
     privileges.fullAccess();
   } else if (user.role === 'MANAGER') {
-    // Managers can read all users, create new users (except ADMIN), and update any user's name/email
-    // can('read', 'User');
-    // cannot('create', 'User');
-    // can('update', 'User');
-    // cannot('delete', 'User');
+    // Managers can read all users, and update any user's name/email
     privileges.readUpdate('User');
+    privileges.readUpdate('Category');
+    privileges.readUpdate('Exhibit');
   } else if (user.role === 'VIEWER') {
     // regular user
-    // can('read', 'User');
-    // cannot('create', 'User');
-    // cannot('update', 'User');
-    // cannot('delete', 'User');
     privileges.readOnly('User');
+    privileges.readOnly('Category');
+    privileges.readOnly('Exhibit');
   }
   return new Ability(rules);
 } 
