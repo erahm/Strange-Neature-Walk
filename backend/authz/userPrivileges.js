6e3f64a651e596961c65aa0db00230e81bf13777
Erich Rahm <erahm84@gmail.com>
2025-12-02 07:53:30 -0700

    removes comments

--- backend/authz/userPrivileges.js
+++ backend/authz/userPrivileges.js
@@ -5,19 +5,16 @@ export function defineAbilitiesForUser(user) {
   const { can, cannot, rules } = new AbilityBuilder(Ability);
   const privileges = privilegeSets(can, cannot);
   if (!user) {
-    // unauthenticated users can read User listings
     privileges.noAccess('User');
     privileges.readOnly('ExhibitCategory');
     privileges.readOnly('Exhibit');
   } else if (user.role === 'ADMIN') {
     privileges.fullAccess();
   } else if (user.role === 'MANAGER') {
-    // Managers can read all users, and update any user's name/email
     privileges.readUpdate('User');
     privileges.readUpdate('ExhibitCategory');
     privileges.readUpdate('Exhibit');
   } else if (user.role === 'VIEWER') {
-    // regular user
     privileges.readOnly('User');
     privileges.readOnly('ExhibitCategory');
     privileges.readOnly('Exhibit');
